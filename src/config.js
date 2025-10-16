dbc27b26991194fa16434db50059a0c381073a7e
tigerbhasha <connects2farm@gmail.com>
2025-10-16 08:51:44 +0530

    Updated project files

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://testing.careerredefine.com"
+    ? "https://test9-jizn.onrender.com"
     : "http://localhost:3000";
 
 export default BASE_URL;
